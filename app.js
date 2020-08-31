96d45e61368b73405b2746301da663e91503a873
Ashutosh Mishra <asho251@gmail.com>
2020-08-31 17:57:20 +0530

    GraphQL Demo

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+  *  The entry file
+  *
+  *  @author   : Gin (gin.lance.inside@hotmail.com)
+  *  @export   : { express }
+  *
+  */
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+
+import schema from './schemas/index'
+
+// create http server
+const app = express()
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
+
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
